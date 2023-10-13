c489f3973e4e0e2f9fdeee3ce92ef3c2d28bb38d
rOwe <100542487+rOwe08@users.noreply.github.com>
2023-10-13 16:25:52 +0200

    Completing the first task of the second lab

---
+++ labs/02-string-revert/02-string-revert.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+std::string revert(std::string inputString) 
+{
+    std::string result;
+
+    for (int i = inputString.length() - 1; i > -1; i--)
+    {
+        result.push_back(inputString[i]);
+    }
+    return result;
+}
+void revertInPlace(std::string& inputString)
+{
+    char tempSymbol;
+    int i = 0;
+    int j = inputString.length() - 1;
+
+    while (i < j)
+    {
+        tempSymbol = inputString[i];
+        inputString[i] = inputString[j];
+        inputString[j] = tempSymbol;
+        i++;
+        j--;
+    }
+}
+
+int main()
+{
+    std::string original = "123456789";
+    auto reverted = revert(original);
+    auto reverted_twice = revert(reverted);
+
+    revertInPlace(original);
+
+    std::cout << "Reverted original (987654321): " << original << std::endl
+        << "Reverted (987654321): " << reverted << std::endl
+        << "Reverted twice (123456789): " << reverted_twice << std::endl;
+}
