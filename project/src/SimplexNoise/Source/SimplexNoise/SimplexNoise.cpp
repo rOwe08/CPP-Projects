2651529763fd4bda3f93679633f5ebf9fd2c829e
rOwe <100542487+rOwe08@users.noreply.github.com>
2024-01-03 15:14:39 +0100

    Creating ue5 project

---
+++ project/src/SimplexNoise/Source/SimplexNoise/SimplexNoise.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SimplexNoise.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SimplexNoise, "SimplexNoise" );
